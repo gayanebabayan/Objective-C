e9515dbd2deeb21dc12a813f55155606e2c41419
gayanebabayan <g.babayan@yahoo.com>
2019-05-01 20:20:19 -0400

    Add files via upload

---
+++ Lab3/List.h
@@ -0,0 +1,39 @@
+// Workshop: Lab-3
+// File: List.h
+// Date: 23-Sept-2018
+// Name: Gayane Babayan
+// ID:   107-061-160
+
+#ifndef SICT_LIST_H
+#define SICT_LIST_H
+
+   template <typename T, int N>
+   class List {
+   private:
+      T m_Array[N];
+      size_t m_count;
+   public:
+      // default constructor
+      List() {
+         m_count = 0;
+      }   
+
+      // returns number of entires in the array
+      size_t size() const {
+         return m_count;
+      }
+
+      // overloaded operator, returns the requested array index
+      const T& operator [] (int index) const {
+            return m_Array[index];
+      }
+
+      // overloaded operator that adds an element to the array if space is available
+      void operator +=(const T& src) {
+         if (size() < N) {
+            m_Array[size()] = src;
+            m_count++;
+         }
+      }
+   };
+#endif 
---
+++ Lab3/Pair.h
@@ -0,0 +1,35 @@
+// Workshop: Lab-3
+// File: Pair.h
+// Date: 23-Sept-2018
+// Name: Gayane Babayan
+// ID:   107-061-160
+
+#ifndef SICT_PAIR_H
+#define SICT_PAIR_H
+
+template <typename A, typename B>
+class Pair {
+private:
+   A m_key1;
+   B m_key2;
+public:
+   // default construction - adopts a safe empty state
+   Pair() {}
+
+   // overloaded constructor that sets the key-value pair elements of the class
+   Pair(const A& value1, const B& value2) {
+      m_key1 = value1;
+      m_key2 = value2;
+   }
+
+   // returns a read-only version of the stored key information
+   const A& getKey() const {
+      return m_key1;
+   }
+
+   // returns a read-only version of the stored value information
+   const B& getValue() const {
+      return m_key2;
+   }
+};
+#endif 
