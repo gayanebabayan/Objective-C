75546d03f4e8e15dd34285a357659de9ae3ff621
gayanebabayan <g.babayan@yahoo.com>
2019-05-01 22:56:55 -0400

    Update SecureData.cpp

--- Lab9/SecureData.cpp
+++ Lab9/SecureData.cpp
@@ -1,12 +1,3 @@
-// Workshop: Lab-9
-// File: SecureData.cpp
-// Date: 17-Nov-2018
-// Name: Gayane Babayan
-// ID:   107-061-160
-
-// Workshop 9 - Multi-Threading
-// SecureData.cpp
-
 #include <iostream>
 #include <fstream>
 #include <string>
